15d40b2c56231578cfe3562175524c563ddbf424
robsonklima <robsonklima@gmail.com>
2023-01-02 17:17:16 -0300

    Try Catch Exception

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ provider = "text-davinci-003"
 openai.api_key = "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"
 sentiment = "Classify the sentiment in these tweets: 1. Estou cansado de trabalhar neste projeto 2. Este bolo me parece bom 3. Estou satisfeito com esta tarefa."
 
-response = openai.Completion.create(
-  model=provider,
-  prompt=classifier,
-  temperature=0,
-  max_tokens=60,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
+try:
+  response = openai.Completion.create(
+    model=provider,
+    prompt=sentiment,
+    temperature=0,
+    max_tokens=60,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
 
-print(response['choices'][0]['text'])
+  print(response)
+except Exception as e:
+  print(e)
